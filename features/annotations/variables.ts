08890b5816bd19f83f292d24f418f989a487a1e4
humanjboof <humanjboof@gmail.com>
2019-07-02 16:12:56 -0400

    annotations

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,4 +1,5 @@
-// simple type examples
+// simple type annotations examples (not needed, we can let type inference do it's job for these)
+// towards the bottom of the page there is examples of when to use annotations
 let apples: number = 5;
 let speed: string = 'fast';
 let hasName: boolean = true;
@@ -35,5 +36,29 @@ const logNumber: (num: number) => void = (num: number) => {
 apples = 10; // this is good because it is a number
 
 
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // { x:10, y:20 };
 
+// 2) When we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+// just an example, really foundWord should just be false at first
+let foundWord: boolean;
 
+words.forEach(word => {
+    if (word === 'green') {
+        return foundWord = true;
+    }
+});
+
+// 3) When we have a variable to have a type that can't be inferred correctly
+let numberss = [-10, -1, 12];
+let positiveNumber: boolean | number = false;
+
+numberss.forEach(num => {
+    if (num > 0) {
+        return positiveNumber = num;
+    }
+});
